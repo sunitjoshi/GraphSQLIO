3e86500e78a6507c24cea27324594761c83c0c50
eagleamulet <eagleamulet@gmail.com>
2015-12-15 08:27:30 -0600

    GraphSQLIO
    GraphSQLIO is a PowerShell script to automate running SQLIO (from
    Microsoft) and visualizing the results graphically. It also stores the
    results locally for later viewing.

---
+++ TestSQLIO.cmd
@@ -0,0 +1,7 @@
+sqlio -kR -s10 -frandom -o8 -b8 -LS -BN -Fparam.txt 
+sqlio -kR -s10 -frandom -o8 -b64 -LS -BN -Fparam.txt
+sqlio -kR -s10 -frandom -o8 -b512 -LS -BN -Fparam.txt
+sqlio -kW -s10 -frandom -o8 -b8 -LS -BN -Fparam.txt 
+sqlio -kW -s10 -frandom -o8 -b64 -LS -BN -Fparam.txt
+sqlio -kW -s10 -fsequential -o8 -b8 -LS -BN -Fparam.txt 
+sqlio -kW -s10 -fsequential -o8 -b64 -LS -BN -Fparam.txt
